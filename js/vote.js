a88ac53d77793858fe4df6ec92e25dc46cc806d2
Micke <micke@mikahl.se>
2018-04-04 16:18:49 +0200

    change ratingClass to ratingID

--- js/vote.js
+++ js/vote.js
@@ -1,4 +1,4 @@
-ratingArray = [3, 4, 7, 10, 9, 3, 4, 5, 8]
+//ratingArray = [3, 4, 7, 10, 9, 3, 4, 5, 8]
 
 function fetchPlayList(playlistId) {
     return fetch('https://folksa.ga/api/playlists/' + playlistId + '?key=flat_eric')
@@ -21,25 +21,30 @@ function calculateAverage(sum, arrayLength) {
 
 
 function displayAverage(average) {
-
     var display = average.toFixed(1)
-
+    document.getElementById('currentRating').innerHTML = "Rating: " + display;
+    
     console.log(display)
 }
 
-displayAverage( 
-    calculateAverage(
-        calculateSum(ratingArray), ratingArray.length
-    ) 
-)
 
 fetchPlayList('5ac37c251c908334d07e95f9')
    .then((array) => {
     displayAverage( calculateAverage( calculateSum(array.ratings), array.ratings.length))  
 });
 
-
-//.then(displayAverage( calculateAverage( calculateSum(array), array.ratings.length)))
-
-
+function votePlaylist(id, ratingNumber) {
+    fetch(`https://folksa.ga/api/playlists/5aae312ee3534b03981f6521/vote`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ rating: 9 });
+    })
+    .then((response) => response.json())
+    .then((playlist) => {
+        console.log(playlist);
+    });
+}
 

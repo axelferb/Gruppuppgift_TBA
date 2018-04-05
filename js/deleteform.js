dc74a840b55e830080b1e43a9d52ed39631cb61c
vctrklndr <victor@nasa.se>
2018-04-05 11:54:24 +0200

    Fixed a simple delete form to delete artists and albums from the API

---
+++ js/deleteform.js
@@ -0,0 +1,35 @@
+function deleteFunction(param, id) {
+    fetch(`https://folksa.ga/api/${param}/${id}?key=flat_eric`, {
+    method: 'DELETE',
+    headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((response) => response.json())
+    .then((artist) => {
+      console.log(artist);
+    });
+}
+
+// Deletes artist by ID
+const deleteArtistButton= document.getElementById("deleteArtistButton");
+
+deleteArtistButton.addEventListener("click", function (e) {
+    const artistID = document.getElementById("artistID");
+    
+    e.preventDefault();
+    deleteFunction('artists', artistID.value);
+    document.getElementById('artistID').value = '';
+});
+
+// Deletes album by ID
+const deleteAlbumButton= document.getElementById("deleteAlbumButton");
+
+deleteAlbumButton.addEventListener("click", function (e) {
+    const albumID = document.getElementById("albumID");
+    
+    e.preventDefault();
+    deleteFunction('albums', albumID.value);
+    document.getElementById('albumID').value = '';
+});

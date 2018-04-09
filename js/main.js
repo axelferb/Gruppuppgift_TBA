e176b694b9b2c88892c4ffb5ddb4aac40acda8a4
Micke <micke@mikahl.se>
2018-04-09 13:30:29 +0200

    working on single fetch

--- js/main.js
+++ js/main.js
@@ -29,6 +29,17 @@ fetch('https://folksa.ga/api/artists?limit=9&sort=desc&key=flat_eric')
         console.log(artist);
         View.displayArtists(artist);
     });
+function addEventListener(){
+    for (i = 0; i < 6; i++) {
+            
+            var moreInfo = document.getElementById(`latestAlbum${[i]}`);
+            moreInfo.addEventListener('click', fetchSingleAlbum.bind(this));
+    }
+}
+
+function fetchSingleAlbum(){
+    console.log("MUmma")
+}
 
 const View = {
     // Diplays the 6 latest albums on the main page.
@@ -37,8 +48,9 @@ const View = {
         var albumList = "";
 
         for (i = 0; i < albumsLimited.length; i++) {
+            
             latestAlbumWrapper.innerHTML += `
-                <div class="latestAlbum">
+                <div class="latestAlbum" id="latestAlbum${[i]}">
                     <img src="${albumsLimited[i].coverImage}" />
                     <div class="albumInfo">
                         <h4> ${albumsLimited[i].title} </h4>
@@ -50,7 +62,9 @@ const View = {
                     </div>
                 </div>
             `
+
         }
+        addEventListener()
     },
     // Diplays the playlists on the main page.
     displayPlaylists: function (playlists) {
